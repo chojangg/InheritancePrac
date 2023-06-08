a79cd79e4c46f1a2a450f4778b7fc49b1139faed
chojangg <s2115@e-mirim.hs.kr>
2023-06-08 14:07:28 +0900

    초기 커밋.

---
+++ InheritancePrac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InheritancePrac
+{
+    internal static class Program
+    {
+        static void Main(String[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+        }
+    }
+}
